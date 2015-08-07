b8038fa9742025eeb3c704b4afc7c8d8ebc71f4a
Josef Strzibny <strzibny@strzibny.name>
2015-08-07 20:27:49 +0200

    Remove trailling whitespace from tests

--- test/test_all.rb
+++ test/test_all.rb
@@ -4,7 +4,7 @@ require 'test-unit'
 require 'formatted_money'
 
 class TestFormattedMoney < Test::Unit::TestCase
-  
+
   def test_conversion_to_cents
     # Defaults to European style
     assert_equal Integer(0), FormattedMoney.cents('0')
@@ -14,7 +14,7 @@ class TestFormattedMoney < Test::Unit::TestCase
     assert_equal Integer(139400056), FormattedMoney.cents('1.394.000,56')
     assert_equal Integer(789999400056), FormattedMoney.cents('7.899.994.000,56')
   end
-  
+
   def test_european_conversion_to_cents
     assert_equal Integer(0), FormattedMoney::European.cents('0')
     assert_equal Integer(0), FormattedMoney::European.cents('0,00')
@@ -23,7 +23,7 @@ class TestFormattedMoney < Test::Unit::TestCase
     assert_equal Integer(139400056), FormattedMoney::European.cents('1.394.000,56')
     assert_equal Integer(789999400056), FormattedMoney::European.cents('7.899.994.000,56')
   end
-  
+
   def test_american_conversion_to_cents
     assert_equal Integer(0), FormattedMoney::American.cents('0')
     assert_equal Integer(0), FormattedMoney::American.cents('0.00')
@@ -32,7 +32,7 @@ class TestFormattedMoney < Test::Unit::TestCase
     assert_equal Integer(139400056), FormattedMoney::American.cents('1,394,000.56')
     assert_equal Integer(789999400056), FormattedMoney::American.cents('7,899,994,000.56')
   end
-  
+
   def test_formatted_amount
     # Defaults to European style
     assert_equal '0,00', FormattedMoney.amount(Integer(0))
@@ -43,14 +43,14 @@ class TestFormattedMoney < Test::Unit::TestCase
     assert_equal '138.555.200,00', FormattedMoney.amount(Integer(13855520000))
     assert_equal '26.897.200,00', FormattedMoney.amount(Integer(2689720000))
   end
-  
+
   def test_formatted_amount_from_string
     # Strings containing only numbers should be fine as well
     assert_equal '0,00', FormattedMoney.amount(String('0'))
     assert_equal '0,00', FormattedMoney.amount(String('00'))
     assert_equal '138.555.200,00', FormattedMoney.amount('13855520000')
   end
-  
+
   def test_europen_formatted_amount
     assert_equal '0,00', FormattedMoney::European.amount(Integer(0))
     assert_equal '0,00', FormattedMoney::European.amount(Integer(00))
@@ -60,7 +60,7 @@ class TestFormattedMoney < Test::Unit::TestCase
     assert_equal '138.555.200,00', FormattedMoney::European.amount(Integer(13855520000))
     assert_equal '26.897.200,00', FormattedMoney::European.amount(Integer(2689720000))
   end
-  
+
   def test_american_formatted_amount
     assert_equal '0.00', FormattedMoney::American.amount(Integer(0))
     assert_equal '0.00', FormattedMoney::American.amount(Integer(00))
@@ -70,69 +70,69 @@ class TestFormattedMoney < Test::Unit::TestCase
     assert_equal '138,555,200.00', FormattedMoney::American.amount(Integer(13855520000))
     assert_equal '26,897,200.00', FormattedMoney::American.amount(Integer(2689720000))
   end
-  
+
   def test_check_float
     FormattedMoney.check_float('123,254')
     FormattedMoney.check_float('3,254.456')
-    
+
 	  assert_raises (ArgumentError) {
       FormattedMoney.check_float('123254d')
     }
 	end
-	
+
 	def test_check_integer
 	  FormattedMoney.check_integer('123254')
 	  FormattedMoney.check_integer(123254)
-	  
+
 	  assert_raises (ArgumentError) {
        FormattedMoney.check_integer('123254d')
     }
 	end
-  
+
   def test_number_with_delimiter
     assert_equal '1.000.000', FormattedMoney.number_with_delimiter('1000000','.')
     assert_equal '400|000', FormattedMoney.number_with_delimiter('400000','|')
     assert_equal '21,000,000,156', FormattedMoney.number_with_delimiter('21000000156',',')
-    
+
     assert_equal '1 000 000', FormattedMoney.number_with_delimiter('1000000',' ')
     assert_equal '400 000', FormattedMoney.number_with_delimiter('400000',' ')
     assert_equal '21 000 000 156', FormattedMoney.number_with_delimiter('21000000156',' ')
-    
+
     assert_equal '1.000.000', FormattedMoney.number_with_delimiter(1000000,'.')
     assert_equal '400|000', FormattedMoney.number_with_delimiter(400000,'|')
     assert_equal '21,000,000,156', FormattedMoney.number_with_delimiter(21000000156,',')
   end
-  
+
   def test_trim_leading_zeros
     assert_equal '0', FormattedMoney.trim_leading_zeros('0')
     assert_equal '525', FormattedMoney.trim_leading_zeros('0525')
     assert_equal '2', FormattedMoney.trim_leading_zeros('0002')
     assert_equal '300', FormattedMoney.trim_leading_zeros('0300')
   end
-  
+
   def test_separator_regex
     FormattedMoney.escape_chars = ['.', '^', '$', '*']
-    
+
     assert_equal '\\.', FormattedMoney.separator_regex('.')
     assert_equal ',', FormattedMoney.separator_regex(',')
-    
+
     FormattedMoney.escape_chars = []
-    
+
     assert_equal '.', FormattedMoney.separator_regex('.')
   end
-  
+
   def test_only_zeros
     delimiter = '\.'
     cents_separator = ','
-    
-    assert_equal true, FormattedMoney.only_zeros?('0', delimiter, cents_separator)   
-    assert_equal true, FormattedMoney.only_zeros?('0,0', delimiter, cents_separator)   
-    assert_equal true, FormattedMoney.only_zeros?('0,00', delimiter, cents_separator)   
-    assert_equal true, FormattedMoney.only_zeros?('0.00', delimiter, cents_separator)   
-    assert_equal true, FormattedMoney.only_zeros?('0.000,0', delimiter, cents_separator)   
-    assert_equal true, FormattedMoney.only_zeros?(',0', delimiter, cents_separator)   
-    assert_equal false, FormattedMoney.only_zeros?('01', delimiter, cents_separator)   
-    assert_equal false, FormattedMoney.only_zeros?('0|0', delimiter, cents_separator)   
-    assert_equal false, FormattedMoney.only_zeros?('000500', delimiter, cents_separator)   
+
+    assert_equal true, FormattedMoney.only_zeros?('0', delimiter, cents_separator)
+    assert_equal true, FormattedMoney.only_zeros?('0,0', delimiter, cents_separator)
+    assert_equal true, FormattedMoney.only_zeros?('0,00', delimiter, cents_separator)
+    assert_equal true, FormattedMoney.only_zeros?('0.00', delimiter, cents_separator)
+    assert_equal true, FormattedMoney.only_zeros?('0.000,0', delimiter, cents_separator)
+    assert_equal true, FormattedMoney.only_zeros?(',0', delimiter, cents_separator)
+    assert_equal false, FormattedMoney.only_zeros?('01', delimiter, cents_separator)
+    assert_equal false, FormattedMoney.only_zeros?('0|0', delimiter, cents_separator)
+    assert_equal false, FormattedMoney.only_zeros?('000500', delimiter, cents_separator)
   end
 end
